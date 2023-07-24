082884a1cbb011d0cfb9fb3b549c6c7ee47c47ba
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-07-24 13:06:05 +0530

    fixed wishlist issue

--- src/app/Modules/Wishlist/Services/WishlistService.php
+++ src/app/Modules/Wishlist/Services/WishlistService.php
@@ -3,12 +3,14 @@
 namespace App\Modules\Wishlist\Services;
 
 use App\Modules\Wishlist\Models\Wishlist;
+use Illuminate\Support\Facades\DB;
 
 class WishlistService
 {
 
     public function get(): Wishlist|null
     {
+        Wishlist::whereNull('user_id')->delete();
         $data = Wishlist::with([
             'products' => function($q) {
                 $q->with(['categories']);
@@ -16,6 +18,10 @@ class WishlistService
         ])->withCount(['products'])->where('user_id', auth()->user()->id)->first();
         if(!$data){
             $data = $data = $this->save(['user_id'=>auth()->user()->id]);
+        }else{
+            DB::table('wishlist_products')->where('wishlist_id', $data->id)->where(function($q){
+                $q->whereNull('product_id');
+            })->delete();
         }
         return $data;
     }
