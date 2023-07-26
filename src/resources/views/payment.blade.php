6927aa587c0a46403250ec11f15072764c3f5644
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-07-27 02:20:44 +0530

    fixed payment issue:

--- src/resources/views/payment.blade.php
+++ src/resources/views/payment.blade.php
@@ -82,7 +82,7 @@
         function setPrice() {
             options = {
                 "key": "{{env('RAZORPAY_KEY')}}", // Enter the Key ID generated from the Dashboard
-                "amount": parseInt({{$total_price}}) * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+                "amount": parseFloat({{$total_price}}) * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
                 "order_id": "{{$order->razorpay_order_id}}",
                 "currency": "INR",
                 "name": "JAP",
